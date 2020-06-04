e16fb983f0702327ff2c9c77fe024e28ceae603a
Igor Kim <igor.skh@gmail.com>
2020-06-04 14:13:44 +0200

    os dependent script

--- fix_toc.sh
+++ fix_toc.sh
@@ -1,1 +1,5 @@
-sed -E -i '' 's/(.+)\.html/\1\//g' public/index.html
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -E -i '' 's/(.+)\.html/\1\//g' public/index.html
+else
+    sed -E -i 's/(.+)\.html/\1\//g' public/index.html
+fi
